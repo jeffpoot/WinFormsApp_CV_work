0c171223fa1130d74404517dde96efc641b241a3
Redd Quielvin Siscon <reddsiscon_22@outlook.com>
2025-11-25 23:21:05 +0800

    summary part 3 redd siscon

--- WinFormsApp_CV_work/Creator_SECTIONS/SUMMARY.Designer.cs
+++ WinFormsApp_CV_work/Creator_SECTIONS/SUMMARY.Designer.cs
@@ -31,6 +31,11 @@
             cuiPanel1 = new CuoreUI.Controls.cuiPanel();
             cuiPanel2 = new CuoreUI.Controls.cuiPanel();
             cuiPanel3 = new CuoreUI.Controls.cuiPanel();
+            cuiButton1 = new CuoreUI.Controls.cuiButton();
+            cuiButton2 = new CuoreUI.Controls.cuiButton();
+            summaryEdit_bttn = new CuoreUI.Controls.cuiButton();
+            cuiButton4 = new CuoreUI.Controls.cuiButton();
+            cuiPanel2.SuspendLayout();
             SuspendLayout();
             // 
             // cuiPanel1
@@ -46,6 +51,10 @@
             // 
             // cuiPanel2
             // 
+            cuiPanel2.Controls.Add(cuiButton4);
+            cuiPanel2.Controls.Add(summaryEdit_bttn);
+            cuiPanel2.Controls.Add(cuiButton2);
+            cuiPanel2.Controls.Add(cuiButton1);
             cuiPanel2.Location = new Point(375, 800);
             cuiPanel2.Name = "cuiPanel2";
             cuiPanel2.OutlineThickness = 1F;
@@ -66,6 +75,154 @@
             cuiPanel3.Size = new Size(300, 75);
             cuiPanel3.TabIndex = 2;
             // 
+            // cuiButton1
+            // 
+            cuiButton1.CheckButton = false;
+            cuiButton1.Checked = false;
+            cuiButton1.CheckedBackground = Color.FromArgb(255, 106, 0);
+            cuiButton1.CheckedForeColor = Color.White;
+            cuiButton1.CheckedImageTint = Color.White;
+            cuiButton1.CheckedOutline = Color.FromArgb(255, 106, 0);
+            cuiButton1.Content = "Your text here!";
+            cuiButton1.DialogResult = DialogResult.None;
+            cuiButton1.Font = new Font("Microsoft Sans Serif", 9.75F);
+            cuiButton1.ForeColor = Color.Black;
+            cuiButton1.HoverBackground = Color.White;
+            cuiButton1.HoverForeColor = Color.Black;
+            cuiButton1.HoverImageTint = Color.White;
+            cuiButton1.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            cuiButton1.Image = null;
+            cuiButton1.ImageAutoCenter = true;
+            cuiButton1.ImageExpand = new Point(0, 0);
+            cuiButton1.ImageOffset = new Point(0, 0);
+            cuiButton1.Location = new Point(100, 25);
+            cuiButton1.Name = "cuiButton1";
+            cuiButton1.NormalBackground = Color.White;
+            cuiButton1.NormalForeColor = Color.Black;
+            cuiButton1.NormalImageTint = Color.White;
+            cuiButton1.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton1.OutlineThickness = 1F;
+            cuiButton1.PressedBackground = Color.WhiteSmoke;
+            cuiButton1.PressedForeColor = Color.FromArgb(32, 32, 32);
+            cuiButton1.PressedImageTint = Color.White;
+            cuiButton1.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton1.Rounding = new Padding(8);
+            cuiButton1.Size = new Size(150, 30);
+            cuiButton1.TabIndex = 0;
+            cuiButton1.TextAlignment = StringAlignment.Center;
+            cuiButton1.TextOffset = new Point(0, 0);
+            // 
+            // cuiButton2
+            // 
+            cuiButton2.CheckButton = false;
+            cuiButton2.Checked = false;
+            cuiButton2.CheckedBackground = Color.FromArgb(255, 106, 0);
+            cuiButton2.CheckedForeColor = Color.White;
+            cuiButton2.CheckedImageTint = Color.White;
+            cuiButton2.CheckedOutline = Color.FromArgb(255, 106, 0);
+            cuiButton2.Content = "Your text here!";
+            cuiButton2.DialogResult = DialogResult.None;
+            cuiButton2.Font = new Font("Microsoft Sans Serif", 9.75F);
+            cuiButton2.ForeColor = Color.Black;
+            cuiButton2.HoverBackground = Color.White;
+            cuiButton2.HoverForeColor = Color.Black;
+            cuiButton2.HoverImageTint = Color.White;
+            cuiButton2.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            cuiButton2.Image = null;
+            cuiButton2.ImageAutoCenter = true;
+            cuiButton2.ImageExpand = new Point(0, 0);
+            cuiButton2.ImageOffset = new Point(0, 0);
+            cuiButton2.Location = new Point(275, 25);
+            cuiButton2.Name = "cuiButton2";
+            cuiButton2.NormalBackground = Color.White;
+            cuiButton2.NormalForeColor = Color.Black;
+            cuiButton2.NormalImageTint = Color.White;
+            cuiButton2.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton2.OutlineThickness = 1F;
+            cuiButton2.PressedBackground = Color.WhiteSmoke;
+            cuiButton2.PressedForeColor = Color.FromArgb(32, 32, 32);
+            cuiButton2.PressedImageTint = Color.White;
+            cuiButton2.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton2.Rounding = new Padding(8);
+            cuiButton2.Size = new Size(75, 30);
+            cuiButton2.TabIndex = 1;
+            cuiButton2.TextAlignment = StringAlignment.Center;
+            cuiButton2.TextOffset = new Point(0, 0);
+            // 
+            // summaryEdit_bttn
+            // 
+            summaryEdit_bttn.CheckButton = false;
+            summaryEdit_bttn.Checked = false;
+            summaryEdit_bttn.CheckedBackground = Color.FromArgb(255, 106, 0);
+            summaryEdit_bttn.CheckedForeColor = Color.White;
+            summaryEdit_bttn.CheckedImageTint = Color.White;
+            summaryEdit_bttn.CheckedOutline = Color.FromArgb(255, 106, 0);
+            summaryEdit_bttn.Content = "Edit";
+            summaryEdit_bttn.DialogResult = DialogResult.None;
+            summaryEdit_bttn.Font = new Font("Microsoft Sans Serif", 9.75F);
+            summaryEdit_bttn.ForeColor = Color.Black;
+            summaryEdit_bttn.HoverBackground = Color.White;
+            summaryEdit_bttn.HoverForeColor = Color.Black;
+            summaryEdit_bttn.HoverImageTint = Color.White;
+            summaryEdit_bttn.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            summaryEdit_bttn.Image = null;
+            summaryEdit_bttn.ImageAutoCenter = true;
+            summaryEdit_bttn.ImageExpand = new Point(0, 0);
+            summaryEdit_bttn.ImageOffset = new Point(0, 0);
+            summaryEdit_bttn.Location = new Point(25, 25);
+            summaryEdit_bttn.Name = "summaryEdit_bttn";
+            summaryEdit_bttn.NormalBackground = Color.White;
+            summaryEdit_bttn.NormalForeColor = Color.Black;
+            summaryEdit_bttn.NormalImageTint = Color.White;
+            summaryEdit_bttn.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            summaryEdit_bttn.OutlineThickness = 1F;
+            summaryEdit_bttn.PressedBackground = Color.WhiteSmoke;
+            summaryEdit_bttn.PressedForeColor = Color.FromArgb(32, 32, 32);
+            summaryEdit_bttn.PressedImageTint = Color.White;
+            summaryEdit_bttn.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            summaryEdit_bttn.Rounding = new Padding(8);
+            summaryEdit_bttn.Size = new Size(50, 30);
+            summaryEdit_bttn.TabIndex = 2;
+            summaryEdit_bttn.TextAlignment = StringAlignment.Center;
+            summaryEdit_bttn.TextOffset = new Point(0, 0);
+            // 
+            // cuiButton4
+            // 
+            cuiButton4.CheckButton = false;
+            cuiButton4.Checked = false;
+            cuiButton4.CheckedBackground = Color.FromArgb(255, 106, 0);
+            cuiButton4.CheckedForeColor = Color.White;
+            cuiButton4.CheckedImageTint = Color.White;
+            cuiButton4.CheckedOutline = Color.FromArgb(255, 106, 0);
+            cuiButton4.Content = "Your text here!";
+            cuiButton4.DialogResult = DialogResult.None;
+            cuiButton4.Font = new Font("Microsoft Sans Serif", 9.75F);
+            cuiButton4.ForeColor = Color.Black;
+            cuiButton4.HoverBackground = Color.White;
+            cuiButton4.HoverForeColor = Color.Black;
+            cuiButton4.HoverImageTint = Color.White;
+            cuiButton4.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            cuiButton4.Image = null;
+            cuiButton4.ImageAutoCenter = true;
+            cuiButton4.ImageExpand = new Point(0, 0);
+            cuiButton4.ImageOffset = new Point(0, 0);
+            cuiButton4.Location = new Point(375, 25);
+            cuiButton4.Name = "cuiButton4";
+            cuiButton4.NormalBackground = Color.White;
+            cuiButton4.NormalForeColor = Color.Black;
+            cuiButton4.NormalImageTint = Color.White;
+            cuiButton4.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton4.OutlineThickness = 1F;
+            cuiButton4.PressedBackground = Color.WhiteSmoke;
+            cuiButton4.PressedForeColor = Color.FromArgb(32, 32, 32);
+            cuiButton4.PressedImageTint = Color.White;
+            cuiButton4.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton4.Rounding = new Padding(8);
+            cuiButton4.Size = new Size(75, 30);
+            cuiButton4.TabIndex = 3;
+            cuiButton4.TextAlignment = StringAlignment.Center;
+            cuiButton4.TextOffset = new Point(0, 0);
+            // 
             // SUMMARY
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -76,6 +233,7 @@
             Controls.Add(cuiPanel1);
             Name = "SUMMARY";
             Size = new Size(1250, 900);
+            cuiPanel2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -84,5 +242,9 @@
         private CuoreUI.Controls.cuiPanel cuiPanel1;
         private CuoreUI.Controls.cuiPanel cuiPanel2;
         private CuoreUI.Controls.cuiPanel cuiPanel3;
+        private CuoreUI.Controls.cuiButton cuiButton2;
+        private CuoreUI.Controls.cuiButton cuiButton1;
+        private CuoreUI.Controls.cuiButton cuiButton4;
+        private CuoreUI.Controls.cuiButton summaryEdit_bttn;
     }
 }
